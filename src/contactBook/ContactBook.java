6020532a95eab501cc0bb0ab09a5ebc9e96438f3
Joao <jpp.vieira@campus.fct.unl.pt>
2021-10-01 15:25:33 +0100

    Implemented equalPhone() method in ContactBook class, as part of the funcionality 9/EP command.

--- src/contactBook/ContactBook.java
+++ src/contactBook/ContactBook.java
@@ -93,4 +93,14 @@ public class ContactBook {
         return contacts[currentContact++];
     }
 
+    public boolean equalPhone() {
+        for(int i = 0; i < counter; i++){
+            for(int j = i + 1; j < counter; j++){
+                if(contacts[i].getPhone() == contacts[j].getPhone())
+                    return true;
+            }
+        }
+        return false;
+    }
+
 }

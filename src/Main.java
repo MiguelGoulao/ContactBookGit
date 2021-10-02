2bdfbce91cf5c6fe02aee142b3761e0da39c183c
Goncalo <gv.martins@campus.fct.unl.pt>
2021-10-02 15:41:35 +0100

    Main com getContact e haveSamePhone

--- src/Main.java
+++ src/Main.java
@@ -14,16 +14,21 @@ public class Main {
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
     public static final String QUIT           = "Q";
+    public static final String GET_CONTACT    = "GN";
+    public static final String SAME_PHONE    = "EP";
 
     //Constantes que definem as mensagens para o utilizador
-    public static final String CONTACT_EXISTS = "contactBook.Contact already exists.";
-    public static final String NAME_NOT_EXIST = "contactBook.Contact does not exist.";
-    public static final String CONTACT_ADDED = "contactBook.Contact added.";
-    public static final String CONTACT_REMOVED = "contactBook.Contact removed.";
-    public static final String CONTACT_UPDATED = "contactBook.Contact updated.";
-    public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
+    public static final String CONTACT_EXISTS = "Contact already exists.";
+    public static final String NAME_NOT_EXIST = "Contact does not exist.";
+    public static final String CONTACT_ADDED = "Contact added.";
+    public static final String CONTACT_REMOVED = "Contact removed.";
+    public static final String CONTACT_UPDATED = "Contact updated.";
+    public static final String BOOK_EMPTY = "Contact book empty.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
+    public static final String PHONE_NOT_EXIST = "Phone number does not exist.";
+    public static final String SHARE_PHONE = "There are contacts that share phone numbers.";
+    public static final String NOT_SHARE_PHONE = "All contacts have different phone numbers";
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -52,6 +57,12 @@ public class Main {
                     break;
                 case LIST_CONTACTS:
                     listAllContacts(cBook);
+                    break;
+                case GET_CONTACT:
+                    getContact(in,cBook);
+                    break;
+                case SAME_PHONE:
+                    haveSamePhone(cBook);
                     break;
                 default:
                     System.out.println(COMMAND_ERROR);
@@ -146,5 +157,23 @@ public class Main {
             }
         }
         else System.out.println(BOOK_EMPTY);
+    }
+
+    private static void getContact(Scanner in, ContactBook cBook) {
+        int phone;
+        String name;
+        phone = in.nextInt();
+        name = cBook.getContact(phone);
+        if(name == null)
+            System.out.println(PHONE_NOT_EXIST);
+        else
+            System.out.println(name);
+    }
+
+    private static void haveSamePhone(ContactBook cBook) {
+        if(cBook.haveSamePhone())
+            System.out.println(SHARE_PHONE);
+        else
+            System.out.println(NOT_SHARE_PHONE);
     }
 }

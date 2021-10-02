5555e3dd6ad18cff9239f5f6ced6921f3ccce5b6
Hugo <hg.pereira@campus.fct.unl.pt>
2021-10-02 16:44:46 +0100

    Initial changes to the project, where were added the new command constants (EP and GN), the corresponding error messages and the header of that methods (searchContact / verifyContact).

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,8 @@ public class Main {
     public static final String SET_PHONE      = "SP";
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
+    public static final String SEARCH_CONTACT = "GN";
+    public static final String VERIFY_CONTACT = "EP";
     public static final String QUIT           = "Q";
 
     //Constantes que definem as mensagens para o utilizador
@@ -24,6 +26,9 @@ public class Main {
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
+    public static final String PHONE_NOT_EXISTS = "Phone number does not exist.";
+    public static final String DIFFERENT_PHONES = "All contacts have different phone numbers.";
+    public static final String SAME_PHONES = "There are contacts that share phone numbers.";
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -52,6 +57,12 @@ public class Main {
                     break;
                 case LIST_CONTACTS:
                     listAllContacts(cBook);
+                    break;
+                case SEARCH_CONTACT:
+                    searchContact(in,cBook);
+                    break;
+                case VERIFY_CONTACT:
+                    verifyContact(cBook);
                     break;
                 default:
                     System.out.println(COMMAND_ERROR);
@@ -147,4 +158,8 @@ public class Main {
         }
         else System.out.println(BOOK_EMPTY);
     }
+
+    public static void searchContact(Scanner in, ContactBook cBook) {}
+
+    public static void verifyContact(ContactBook cBook) {}
 }

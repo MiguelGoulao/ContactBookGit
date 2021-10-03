7d81141a1fcdbdd53c603997d4f0072ab2188fa4
António Duarte <58123364+antonionduarte@users.noreply.github.com>
2021-10-03 21:54:02 +0100

    Added main method for EP command (check for duplicate contacts)

--- src/Main.java
+++ src/Main.java
@@ -14,6 +14,7 @@ public class Main {
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
     public static final String QUIT           = "Q";
+    public static final String DUPLICATE_PHONES = "EP";
 
     //Constantes que definem as mensagens para o utilizador
     public static final String CONTACT_EXISTS = "contactBook.Contact already exists.";
@@ -22,6 +23,8 @@ public class Main {
     public static final String CONTACT_REMOVED = "contactBook.Contact removed.";
     public static final String CONTACT_UPDATED = "contactBook.Contact updated.";
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
+    public static final String CONTACTS_WITH_SAME_PHONE = "There are contacts that share phone numbers.";
+    public static final String ALL_CONTACTS_DIFFERENT_PHONE = "All contacts have different phone numbers";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
 
@@ -52,6 +55,9 @@ public class Main {
                     break;
                 case LIST_CONTACTS:
                     listAllContacts(cBook);
+                    break;
+                case DUPLICATE_PHONES:
+                    duplicatePhones(cBook);
                     break;
                 default:
                     System.out.println(COMMAND_ERROR);
@@ -146,5 +152,13 @@ public class Main {
             }
         }
         else System.out.println(BOOK_EMPTY);
+    }
+
+    private static void duplicatePhones(ContactBook cBook) {
+        if (cBook.hasDuplicatePhones()) {
+            System.out.println(CONTACTS_WITH_SAME_PHONE);
+        } else {
+            System.out.println(ALL_CONTACTS_DIFFERENT_PHONE);
+        }
     }
 }

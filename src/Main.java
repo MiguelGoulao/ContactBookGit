dbd82d86908068a04df30ea986f47c33b69f1cd5
Yuliia <y.bila@campus.fct.unl.pt>
2021-10-03 13:46:19 +0100

    COMENTARIO

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main {
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
     public static final String QUIT           = "Q";
-
+    //NEW
     //Constantes que definem as mensagens para o utilizador
     public static final String CONTACT_EXISTS = "contactBook.Contact already exists.";
     public static final String NAME_NOT_EXIST = "contactBook.Contact does not exist.";

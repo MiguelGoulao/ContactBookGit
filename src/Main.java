f999a6019c670ecd86c723577f939df2c883bfd5
CJSousa <cjo.sousa@campus.fct.unl.pt>
2021-10-05 11:06:09 +0100

    Preparing new commands.

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,8 @@ public class Main {
     public static final String SET_PHONE      = "SP";
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
+    public static final String GET_NUMBER     = "GN";
+    public static final String SAME_PHONE     = "EP";
     public static final String QUIT           = "Q";
 
     //Constantes que definem as mensagens para o utilizador
@@ -22,6 +24,9 @@ public class Main {
     public static final String CONTACT_REMOVED = "contactBook.Contact removed.";
     public static final String CONTACT_UPDATED = "contactBook.Contact updated.";
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
+    public static final String NUMBER_NOT_EXIST = "Phone number does not exist.";
+    public static final String SAME_NUMBER = "There are contacts that share phone numbers.";
+    public static final String DIFF_PHONES = "All contacts have different phone numbers.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
 
@@ -52,6 +57,12 @@ public class Main {
                     break;
                 case LIST_CONTACTS:
                     listAllContacts(cBook);
+                    break;
+                case GET_NUMBER:
+                    getNumber(in, cBook);
+                    break;
+                case SAME_PHONE:
+                    lookForSameNumber(cBook);
                     break;
                 default:
                     System.out.println(COMMAND_ERROR);
@@ -147,4 +158,23 @@ public class Main {
         }
         else System.out.println(BOOK_EMPTY);
     }
+
+    /**
+     * Finds a contact given a phone number, if it exists in the system.
+     *
+     * @param in - scanner
+     * @param cBook - repository of contacts
+     */
+    private static void getNumber(Scanner in, ContactBook cBook) {
+
+    }
+
+    /**
+     * Checks if there are any contacts with the same phone number.
+     *
+     * @param cBook - repository of contacts
+     */
+    private static void lookForSameNumber(ContactBook cBook) {
+    }
+
 }

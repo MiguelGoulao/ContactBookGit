5b738efc4463507504c027f75f78014dfadbfb5d
jtdsantos <jtd.santos@campus.fct.unl.pt>
2021-10-05 20:19:34 +0100

    Added method "getContactWithNumber" in class Main

--- src/Main.java
+++ src/Main.java
@@ -24,6 +24,7 @@ public class Main {
     public static final String CONTACT_REMOVED = "contactBook.Contact removed.";
     public static final String CONTACT_UPDATED = "contactBook.Contact updated.";
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
+    public static final String PHONE_DOESNT_EXIST = "Phone number does not exist.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
 
@@ -159,7 +160,13 @@ public class Main {
     }
 
     private static void getContactWithNumber(Scanner in, ContactBook cBook){
-
+        String phone;
+        phone = in.nextLine();
+        String name = cBook.getNameWithNumber(phone);
+        if(name.equals(null)){
+            System.out.println(PHONE_DOESNT_EXIST);
+        }
+        else System.out.println(name);
     }
 
 

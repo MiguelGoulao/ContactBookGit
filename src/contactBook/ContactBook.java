3608b421d53fa144059a20e853e80e9bd7716394
JoanaRitaCruz <jrf.cruz@campus.fct.unl.pt>
2022-09-16 17:06:17 +0100

    With EP method done on ContactBook class

--- src/contactBook/ContactBook.java
+++ src/contactBook/ContactBook.java
@@ -73,6 +73,19 @@ public class ContactBook {
         return result;
     }
 
+    public int searchIndexPhone(int phone) {
+        int i = 0;
+        int result = -1;
+        boolean found = false;
+        while (i<counter && !found)
+            if (contacts[i].getPhone() == phone)
+                found = true;
+            else
+                i++;
+        if (found) result = i;
+        return result;
+    }
+
     private void resize() {
         Contact tmp[] = new Contact[2*contacts.length];
         for (int i=0;i<counter; i++)

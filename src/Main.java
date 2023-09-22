95c68a213ae33ed2152e9ee78a0e6d5c55106790
david <djc.castro@campus.fct.unl.pt>
2023-09-22 12:04:09 +0100

    o maior

--- src/Main.java
+++ src/Main.java
@@ -15,6 +15,8 @@ public class Main {
     public static final String LIST_CONTACTS  = "LC";
     public static final String QUIT           = "Q";
 
+    public static final String DAVID          = "DAVID";
+
     //Constantes que definem as mensagens para o utilizador
     public static final String CONTACT_EXISTS = "contactBook.Contact already exists.";
     public static final String NAME_NOT_EXIST = "contactBook.Contact does not exist.";

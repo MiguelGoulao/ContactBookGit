f4f0275e287b106a2d3e4df5bb2e8517777a8ff2
Nicolas Nascimento <nc.nascimento@campus.fct.unl.pt>
2024-09-12 17:45:24 +0100

    Added EP command in according to what was asked in the assigment

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,7 @@ public class Main {
     public static final String SET_PHONE      = "SP";
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
+    public static final String SEARCH_CONTACT = "EP";
     public static final String QUIT           = "Q";
 
     //Constantes que definem as mensagens para o utilizador
@@ -22,6 +23,8 @@ public class Main {
     public static final String CONTACT_REMOVED = "contactBook.Contact removed.";
     public static final String CONTACT_UPDATED = "contactBook.Contact updated.";
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
+    public static final String EQUAL_CONTACT = "There are contacts that share phone numbers.";
+    public static final String DIFFERENT_CONTACT = "All contacts have different phone numbers.";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
 
@@ -52,6 +55,9 @@ public class Main {
                     break;
                 case LIST_CONTACTS:
                     listAllContacts(cBook);
+                    break;
+                case SEARCH_CONTACT:
+                    searchContact(cBook);
                     break;
                 default:
                     System.out.println(COMMAND_ERROR);
@@ -147,4 +153,25 @@ public class Main {
         }
         else System.out.println(BOOK_EMPTY);
     }
+
+    private static void searchContact(ContactBook cBook) {
+        if (cBook.getNumberOfContacts() != 0) {
+            cBook.initializeIterator();
+            boolean equal = false;
+            while( cBook.hasNext() && !equal ) {
+                Contact c = cBook.next();
+                if (cBook.getPhone(c.getName()) == cBook.getPhone(c.getName())) {
+                    equal = true;
+                }
+            }
+            if (equal) {
+                System.out.println(EQUAL_CONTACT);
+            }
+            else {
+                System.out.println(DIFFERENT_CONTACT);
+            }
+        }
+        else System.out.println(BOOK_EMPTY);
+    }
+
 }

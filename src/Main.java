e1a41a44866decb998944734aaf0ad75005a2ca5
Francisco <fs.almeida@campus.fct.unl.pt>
2024-09-15 16:05:12 +0100

    Added all the static String values to call the missing operations and to be returned by them.

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,8 @@ public class Main {
     public static final String SET_PHONE      = "SP";
     public static final String SET_EMAIL      = "SE";
     public static final String LIST_CONTACTS  = "LC";
+    public static final String GET_CONTACT    = "GN";
+    public static final String CHECK_PHONE    = "EP";
     public static final String QUIT           = "Q";
 
     //Constantes que definem as mensagens para o utilizador
@@ -22,6 +24,9 @@ public class Main {
     public static final String CONTACT_REMOVED = "contactBook.Contact removed.";
     public static final String CONTACT_UPDATED = "contactBook.Contact updated.";
     public static final String BOOK_EMPTY = "contactBook.Contact book empty.";
+    public static final String NO_PHONE = "Phone number does not exist.";
+    public static final String SHARED_PHONE_NUMBERS = "There are contacts that share phone numbers.";
+    public static final String NO_SHARED_PHONE_NUMBERS = "All contacts have different phone numbers";
     public static final String QUIT_MSG = "Goodbye!";
     public static final String COMMAND_ERROR = "Unknown command.";
 
@@ -147,4 +152,5 @@ public class Main {
         }
         else System.out.println(BOOK_EMPTY);
     }
+
 }

8a18bc8c35b7d2779e95d02f952e74275486dd6f
Sofia <s.diogo@campus.fct.unl.pt>
2024-09-16 22:04:52 +0100

    Adicionei o GN.

--- src/contactBook/ContactBook.java
+++ src/contactBook/ContactBook.java
@@ -93,4 +93,14 @@ public class ContactBook {
         return contacts[currentContact++];
     }
 
+
+    public Contact getNumber (int phone){
+        for(int i = 0; i<counter; i++){
+            if(contacts[i].getPhone() == phone){
+                return contacts[i];
+            }
+        }
+        return null;
+    }
+
 }

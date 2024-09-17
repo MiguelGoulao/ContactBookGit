0c3b95972d8f87a60cf73eba67bb5a6c6bf3f271
Joao <j.rivera@campus.fct.unl.pt>
2024-09-17 16:30:27 +0100

    FALTAVA UM PONTO FINAL XD

--- src/Main.java
+++ src/Main.java
@@ -28,7 +28,7 @@ public class Main {
     public static final String COMMAND_ERROR = "Unknown command.";
     public static final String PHONE_NOT_EXIST = "Phone number does not exist.";
     public static final String CONTACTS_SHARE_NUMBER = "There are contacts that share phone numbers.";
-    public static final String CONTACTS_DIFFERENT_NUMBERS = "All contacts have different phone numbers";
+    public static final String CONTACTS_DIFFERENT_NUMBERS = "All contacts have different phone numbers.";
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
